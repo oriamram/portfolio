333a6272982fe4ea6fa713bdfc6749ea659e3f0c
oriamram <oriamram111@gmail.com>
2023-05-25 15:38:21 +0300

    fixed server

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import { Path } from "three";
 const app = express();
 
 app.use(express.static("dist"));
+const port = process.env.PORT || 4000;
 
 app.get("*", (req, res) => {
 	res.sendFile(Path.join(__dirname, "dist", "index.html"));
 });
 
-const port = 5000;
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
 	console.log(`Server is running on port ${port}`);
 });
